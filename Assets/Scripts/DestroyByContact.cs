61eb4e91c894103ea4b4c623387fcbedbf2188fd
RB4F505_007\Administrator <kanyarat.bual@bumail.net>
2017-03-25 14:12:26 +0700

    14.Counting points and displaying the score

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
-	public GameObject explosion;
-	public GameObject playerExplosion;
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "Boundary") {
-			return;
-		}
-		Instantiate (explosion, transform.position, transform.rotation);
-		if (other.tag == "Player") 
-		{
-			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
-		}
+    void Start() 
+    {
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
 
-		Destroy (other.gameObject);
-		Destroy (gameObject);
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+
+        Instantiate(explosion, other.transform.position, other.transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
 }
